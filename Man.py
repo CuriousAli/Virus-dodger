5202fc49422e0e52c2e0abb9485e31311987f8bb
Ali <sadrali1990@gmail.com>
2021-02-02 22:54:48 +0300

    Man(circle) moving by control from keyboard's arrows. for smoothly motion need to whole time move mouse on game window. Realisation of my game turned out more difficult than i expected. Unluko. Still don't understand how realase this by usage classes

--- Man.py
+++ Man.py
@@ -3,45 +3,61 @@ from random import randint
 import sys
 
 
-pg.init()
-
-
 SIZE = 800, 600
-n = randint(1,15)
+rad = randint(10,20)
+x = randint(rad, 800 - rad)
+y = randint(rad, 600 - rad)
 
-def rand_color():
-    return (randint(0,255),randint(0,255),randint(0,255))
-
-
-class Man:
-    def __init__(self, color, x, y, rad = randint(10,20)):
-        x, y = randint(rad, 800 - rad), randint(rad, 600 - rad)
-        color = rand_color()
-        self.x = x
-        self.y = y
-        self.color = color
-        self.rad = rad
-
-    def draw(self, screen):
-        pg.draw.circle(screen, self.rad, (self.x, self.y), self.rad)
 
 
 TITLE = "Test circle"
 screen = pg.display.set_mode(SIZE)
 pg.display.set_caption(TITLE)
 
+
+def rand_color():
+    return (randint(0,255),randint(0,255),randint(0,255))
+
+
+color = rand_color()
+color2 = rand_color()
+
+
+
 pg.display.flip()
 clock = pg.time.Clock()
 
 
-game = True
-
-while game:
+play = True
+while play:
 
 
     for i in pg.event.get():
-        if i.type == pg.quit():
-            game = False
+        if i.type == pg.QUIT:
+            sys.exit()
+
+        screen.fill(color2)
+
+        pg.draw.circle(screen, color, (x, y), rad)
+        pg.display.flip()
+
+        pkgp = pg.key.get_pressed()
+        u = pkgp[pg.K_UP]
+        d = pkgp[pg.K_DOWN]
+        l = pkgp[pg.K_LEFT]
+        r = pkgp[pg.K_RIGHT]
+
+        ''' Circle moves more smoothly if mouse is moving on game window/ Dunno why it happens'''
+
+        if u == True and y >= 30:
+            y -= 10
+        elif d == True and y <= 570:
+            y += 10
+        elif l == True and x >= 30:
+            x -= 10
+        elif r == True and x <= 770:
+            x += 10
 
 
+        clock.tick(60)
 
